ceb79a8ab1fd0960923e73ca067ea52d80008c71
JaqiKal <jaqika@gmail.com>
2025-02-06 15:19:41 +0100

    feat: Add Hero section with intro and CTA
    reated a Hero section with an introduction and call-to-action.
    
    Includes a heading, description, and 'View my work' & 'View my portfolio' button.
    
    Styled using TailwindCSS and supports dark mode.

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <section className="min-h-screen flex flex-col items-center justify-center text-center bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-100 px-6">
+      <h1 className="text-5xl font-heading text-primary-500">Hi, I'm Jacqueline Kalmár</h1>
+      <p className="mt-4 text-lg text-neutral-600 dark:text-neutral-300">A logistician & transport leader and also a junior web developer</p>
+      <div className="mt-6 flex flex-col sm:flex-row gap-4">
+        <button className="px-6 py-2 rounded-lg bg-primary-700 text-white text-lg font-semibold shadow-md hover:bg-success transform hover:scale-105 transition">View My Work</button>
+        <button className="px-6 py-2 rounded-lg bg-primary-700 text-white text-lg font-semibold shadow-md hover:bg-success transform hover:scale-105 transition">View Resume</button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
