60c62b34ef1d993c50202547473fd1c675235245
JaqiKal <jaqika@gmail.com>
2025-02-08 23:01:24 +0100

    feat: Add color sky to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,7 @@ export default {
         warning: "#F59E0B",
         error: "#EF4444",
         info: "#3B82F6",
+        sky: "#bae6fd",
         neutral: {
           100: "#F3F4F6",
           200: "#E5E7EB",
